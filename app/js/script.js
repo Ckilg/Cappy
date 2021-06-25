2bdf7e24867b00148a62f07604d1d23e17786c32
CORP\WESYAO <we.shi.yao@capgemini.com>
2021-06-25 15:16:39 +0200

    Question array added

--- app/js/script.js
+++ app/js/script.js
@@ -3,3 +3,16 @@ if ('serviceWorker' in navigator) {
      navigator.serviceWorker.register('/sw.js');
   });
 }
+
+const questions = [];
+questions[0]= "Is Bob Muller wearing any pants today?";
+questions[1]= "Have you asked Jeffrey how the arve is?";
+questions[2]= "Did you have a coffee break with a colleague?";
+questions[3]= "Is your CV still marked as green?";
+questions[4]= "Have you had a Jumbo date with a colleague?";
+questions[5]= "Have you send a LinkedIn mail to at least one high school classmates";
+questions[6]= "Have you obtained your Scrum certificate yet?";
+questions[7]= "Have you done all your mandatory meetings?";
+
+let randomQuestion = questions[Math.floor(Math.random() * questions.length)];
+console.log(randomQuestion);
